6d8be4baf451265f3cf8198510073cb402ed4686
Noldia Chavez <ivar.n.c.29@gmail.com>
2024-03-11 22:06:26 -0500

    Selectors project

---
+++ src/app/countries/countries.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { CountriesRoutingRoutingModule } from './countries-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    SelectorPageComponent
+  ],
+  imports: [
+    CommonModule,
+    CountriesRoutingRoutingModule,
+    ReactiveFormsModule
+  ]
+})
+export class CountriesModule { }
